e0ff9fcb62d04b08c9fb71c154f08dd68c9ccc8f
sadalasreya <sadalasreyareddy@hmail.comgit config --global user.name sadala>
2023-03-18 06:45:18 +0530

    arthimeticoperations

--- Desktop/python programs/first.py
+++ Desktop/python programs/first.py
@@ -1,1 +1,11 @@
 print("hello")
+a=10
+b=20
+sum=a+b
+mul=a*b
+sub=a-b
+div=a/b
+print("sum of two numbers is:",sum)
+print("mul of two numbers is:",mul)
+print("sub of two numbers is:",sub)
+print("div of two numbers is:",div)
